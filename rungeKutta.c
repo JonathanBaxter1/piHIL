72a2fb99c6ee67a885356f0c4ce27211c4adfea5
Jonathan Baxter <jdbaxte3@ncsu.edu>
2024-09-28 19:29:47 -0400

    i forgot

--- rungeKutta.c
+++ rungeKutta.c
@@ -2,25 +2,38 @@
 #include <math.h>
 
 #define ODE_ORDER 4
-#define STOP_TIME 5
-#define STEP_SIZE 1.0/512.0
+#define STOP_TIME 3.5
+#define STEP_SIZE 1.0/1024.0
 
 #define HOVER_THROTTLE 0.5
 
-void arrayTimesScalar(float *arrayOut, float *arrayIn, float scalar, int size) {
+uint64_t getTime(void) {
+	uint64_t curTime;
+	asm volatile("mrs %0, cntvct_el0" : "=r"(curTime));
+	return curTime;
+}
+
+// Should be 54 Mhz on Rpi 4
+uint64_t getClockFreq(void) {
+	uint64_t clockFreq;
+	asm volatile("mrs %0, cntfrq_el0" : "=r"(clockFreq));
+	return clockFreq;
+}
+
+void arrayTimesScalar(double *arrayOut, double *arrayIn, double scalar, int size) {
 	for (int i = 0; i < size; i++) {
 		arrayOut[i] = arrayIn[i]*scalar;
 	}
 }
 
-void arrayPlusArray(float *arrayOut, float *arrayIn1, float *arrayIn2, int size) {
+void arrayPlusArray(double *arrayOut, double *arrayIn1, double *arrayIn2, int size) {
 	for (int i = 0; i < size; i++) {
 		arrayOut[i] = arrayIn1[i] + arrayIn2[i];
 	}
 }
 
 // Sets a minimum and maximum bound for a value
-float saturation(float value, float min, float max) {
+double saturation(double value, double min, double max) {
 	if (value >= max) {
 		return max;
 	} else if (value <= min) {
@@ -30,34 +43,60 @@ float saturation(float value, float min, float max) {
 	}
 }
 
+// Simulates the MPU-9250's Accerlerometer
+double accelerometer(double rollAngle) {
+	double g = 9.81;
+	double yAcc = g*cos(rollAngle);
+	// Also need to add noise to this (change)
+	return yAcc;
+}
+
+// Simulates the MPU-9250's Gyroscope
+double gyro(double rollRate) {
+	return rollRate; // add noise (change)
+}
+
 // Simulates the drone's flight controller
-float droneController(float t, float gyroData, float accData) {
-	float rollSignal = (t > 1 && t < 1.2)*0.1;
-	return rollSignal;
+double droneController(double t, double gyroData, double accData) {
+	// PID gains
+	double Pgain = 0.015;
+	double Igain = 0.0;
+	double Dgain = 0.0;
+
+	double reference = (fmodl(t, 10.0) < 5.0)*0.75;
+	double rollSignal = gyroData;
+	double pidInput = reference - rollSignal;
+
+	// PID calculation
+	double pidOutput = pidInput*Pgain;
+	return pidOutput;
 }
 
 // Drone Dynamics
-void odes(float *dy, float t, float y[ODE_ORDER]) {
-	float throttle = HOVER_THROTTLE;
-	float roll = droneController(t, 0.0, 0.0);
-	float motor1Throttle = throttle + roll; // just get these from drone
-	float motor2Throttle = throttle - roll;
-	float motor3Throttle = throttle + roll;
-	float motor4Throttle = throttle - roll;
+void odes(double *dy, double t, double y[ODE_ORDER]) {
+	double throttle = HOVER_THROTTLE;
+	double accData = accelerometer(y[0]);
+	double gyroData = gyro(y[0]);
+	double roll = droneController(t, gyroData, accData);
+	double motor1Throttle = throttle + roll; // just get these from drone (change)
+	double motor2Throttle = throttle - roll;
+	double motor3Throttle = throttle + roll;
+	double motor4Throttle = throttle - roll;
 
 	// Check if motors have correct throttle relative to each other, then just use motor1
+	// (change)
+	double motor1LPFinput = saturation(motor1Throttle - 0.08, 0, 0.9)*22.5;
+	double motor2LPFinput = saturation(motor2Throttle - 0.08, 0, 0.9)*22.5;
 
-	float motor1LPFinput = saturation(motor1Throttle - 0.08, 0, 0.9)*22.5;
-	float motor2LPFinput = saturation(motor2Throttle - 0.08, 0, 0.9)*22.5;
+	double motorLPFw = 7.8; // cutoff angular frequency of LPF that describes motor/prop
+	double droneRadius = 0.138; // Meters
+	double momentOfInertia = 0.01147; // Kg*m^2
+//	double Cd = 0.53;
+	double Cd = 9.53; // change
+	double area = 0.2; // Propeller area
+	double rho = 1.225; // Air density (Kg/m^3)
 
-	float motorLPFw = 7.8; // cutoff angular frequency of LPF that describes motor/prop
-	float droneRadius = 0.138; // Meters
-	float momentOfInertia = 0.01147; // Kg*m^2
-	float Cd = 0.53;
-	float area = 0.2; // Propeller area
-	float rho = 1.225; // Air density (Kg/m^3)
-
-	float damping = y[1]*fabs(y[1])*0.5*rho*Cd*area*droneRadius;
+	double damping = y[1]*fabs(y[1])*0.5*rho*Cd*area*droneRadius;
 
 	dy[0] = y[1]; // Derivative of Angular Position
 	dy[1] = ((y[2]*2 - y[3]*2)*droneRadius - damping)/momentOfInertia; // Derivative of Angular Velocity
@@ -66,14 +105,14 @@ void odes(float *dy, float t, float y[ODE_ORDER]) {
 }
 
 // Runge-Kutta ODE Solver
-void rungeKutta(float y[ODE_ORDER], float stepSize, float stopTime) {
-	float dt = stepSize;
-	float k1[ODE_ORDER], k2[ODE_ORDER], k3[ODE_ORDER], k4[ODE_ORDER], dydt[ODE_ORDER];
-	float k1a[ODE_ORDER], k2a[ODE_ORDER], k3a[ODE_ORDER]; // Temporary Variables
+void rungeKutta(double y[ODE_ORDER], double stepSize, double stopTime) {
+	double dt = stepSize;
+	double k1[ODE_ORDER], k2[ODE_ORDER], k3[ODE_ORDER], k4[ODE_ORDER], dydt[ODE_ORDER];
+	double k1a[ODE_ORDER], k2a[ODE_ORDER], k3a[ODE_ORDER]; // Temporary Variables
 
 	// This function uses variable size arrays, which may be a lot slower than doing each
 	// individually. Idk though, I haven't looked at the assembly.
-	float t = 0; // Time
+	double t = 0; // Time
 	while (t < stopTime) {
 		// Calculate 4 approximations for slope
 		odes(k1, t, y);
@@ -92,14 +131,15 @@ void rungeKutta(float y[ODE_ORDER], float stepSize, float stopTime) {
 			dydt[i] = dt/6*(k1[i] + 2*k2[i] + 2*k3[i] + k4[i]);
 			y[i] += dydt[i];
 		}
+		printf("Time: %.6f, Value: %.20f\n", t, y[0]);
 		t += dt;
 
 	}
-	printf("Time: %.6f, Value: %.20f\n", t, y[0]);
+	printf("(Final) Time: %.6f, Value: %.20f\n", t, y[0]);
 }
 
 int main(void) {
-	float initialConditions[ODE_ORDER] = {0, 0, HOVER_THROTTLE, HOVER_THROTTLE};
+	double initialConditions[ODE_ORDER] = {0, 0, HOVER_THROTTLE, HOVER_THROTTLE};
 	rungeKutta(initialConditions, STEP_SIZE, STOP_TIME);
 	return 0;
 }
